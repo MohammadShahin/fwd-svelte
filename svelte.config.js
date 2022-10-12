287fb73e8884f3f3c62868b4971e51b7c02f4bf5
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-10-12 22:46:18 +0300

    fixed svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 
-const dev = process.env.NODE_ENV === 'development';
+// const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,13 +11,10 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({
-            pages: "docs",
-            assets: "docs"
-        }),
-		paths: {
-			base: dev ? '' : '/fwd-svelte',
-		},
+		adapter: adapter(),
+		// paths: {
+		// 	base: dev ? '' : '/fwd-svelte',
+		// },
 	}
 };
 
