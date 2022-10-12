94b97a6771834c029a2bd70f099b5c7a27236597
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-10-12 15:10:41 +0300

    Fixed .nojekyll naming issues and rebuilt

--- docs/_app/immutable/chunks/0-893e7fb5.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-17337c08.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-c8942905.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-83f251ea.js";export{t as component};
