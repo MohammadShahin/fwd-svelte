0162a39067e571010204572373a97ada521cdf60
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-10-24 23:32:21 +0300

    Add tests with lintstager & husky

--- tests/test.ts
+++ tests/test.ts
@@ -1,8 +1,29 @@
-// import { expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-// test('about page has expected h1', async ({ page }) => {
-// 	// await page.goto('/about');
-// 	// expect(await page.textContent('h1')).toBe('About this app');
-// });
+test.describe.configure({ mode: 'parallel' });
 
-export {};
+test('Education page has expected heading2', async ({ page }) => {
+	await page.goto('/education');
+	expect(await page.textContent('.heading2')).toBe('Education');
+});
+
+test('Technical Skills page has expected heading2', async ({ page }) => {
+	await page.goto('/tech-skills');
+	expect(await page.textContent('.heading2')).toBe('Technical Skills');
+});
+
+test('Home page has expected heading4 (sol price)', async ({ page }) => {
+	await page.goto('/');
+	await page.waitForLoadState('networkidle');
+	expect(await page.textContent('.heading4')).toContain('Here goes the conversion rate of Sol');
+});
+
+test('Animation box exists and alerts when clicked', async ({ page }) => {
+	await page.goto('/');
+	expect(await page.textContent('#css-animation')).toBe('Hover');
+	page.on('dialog', async (dialog) => {
+		expect(dialog.message()).toEqual('I said hover not click!');
+		await dialog.accept();
+	});
+	await page.locator('#css-animation').click({ force: true });
+});
